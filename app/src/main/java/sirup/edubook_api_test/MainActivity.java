2c999c94aa5d22506b3b1310596cc6807c9939d9
Mahboi <alexandre.weber@epitech.eu>
2018-08-21 18:13:14 +0200

    Layouts always eat part of the last row. Mass deleted Request classes and set all in a fat MainActivity

--- app/src/main/java/sirup/edubook_api_test/MainActivity.java
+++ app/src/main/java/sirup/edubook_api_test/MainActivity.java
@@ -1,10 +1,15 @@
 package sirup.edubook_api_test;
 
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,6 +17,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import static com.android.volley.VolleyLog.TAG;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     private FrameLayout frameLayout1;
     private FrameLayout frameLayout2;
     private TextView appTitle;
+    private final MainActivity mainActivity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +52,36 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.setPrimaryNavigationFragment(chaptersFragment);
         fragmentTransaction.commit();
 
-        new ChaptersRequest(this).queryChapters(this);
+        queryChapters();
+    }
+
+    public void queryChapters() {
+        String bookUrl = "https://api.lelivrescolaire.fr/public/books/1339497/chapters";
+        Response.Listener<JSONArray> response = new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if (response.length() > 0) {
+                    Log.d(TAG, "response :" + response);
+                    JSONArray uprightResponse = new JSONArray();
+                    try {
+                        for (int i = response.length() - 1; i >= 0; i--) {
+                            uprightResponse.put(response.get(i));
+                        }
+                    } catch (JSONException e) {
+                        Log.d("ChaptersAdapter: ", e.getMessage(), e);
+                    }
+                    RecyclerView recyclerView = findViewById(R.id.Chapters_List);
+                    LinearLayoutManager manager = new LinearLayoutManager(getParent());
+                    recyclerView.setLayoutManager(manager);
+                    recyclerView.setAdapter(new ChaptersAdapter(uprightResponse, mainActivity));
+                    recyclerView.addItemDecoration(new DividerItemDecoration(mainActivity, DividerItemDecoration.VERTICAL));
+                    recyclerView.setHasFixedSize(true);
+                } else {
+                    Toast.makeText(getParent(), "No chapters could be found.", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+        myHttpRequest.queryJSONArray(bookUrl, response);
     }
 
     public void toLessonsFragment(String lessonsURL, String chapterName) {
@@ -49,7 +92,37 @@ public class MainActivity extends AppCompatActivity {
         frameLayout2.setVisibility(View.VISIBLE);
         fragmentTransaction.show(lessonsFragment);
         fragmentTransaction.commit();
-        new LessonsRequest(this).queryLessons(lessonsURL);
+        queryLessons(lessonsURL);
+    }
+
+    public void queryLessons(String chapterID) {
+        String Url = "https://api.lelivrescolaire.fr/public/chapters/";
+        Response.Listener<JSONArray> response = new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, "response :" + response);
+                JSONArray uprightResponse = new JSONArray();
+                try {
+                    for (int i = response.length() - 1; i >= 0; i--) {
+                        uprightResponse.put(response.get(i));
+                    }
+                } catch (JSONException e) {
+                    Log.d("ChaptersAdapter: ", e.getMessage(), e);
+                }
+                if (response.length() > 0) {
+                    RecyclerView recyclerView;
+                    recyclerView = findViewById(R.id.Lesson_List);
+                    LinearLayoutManager manager = new LinearLayoutManager(mainActivity);
+                    recyclerView.setLayoutManager(manager);
+                    recyclerView.setAdapter(new LessonsAdapter(uprightResponse));
+                    recyclerView.addItemDecoration(new DividerItemDecoration(mainActivity, DividerItemDecoration.VERTICAL));
+                    recyclerView.setHasFixedSize(true);
+                } else {
+                    Toast.makeText(mainActivity, "No lessons could be found for this chapter.", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+        myHttpRequest.queryJSONArray(Url + chapterID + "/lessons", response);
     }
 
     @Override
